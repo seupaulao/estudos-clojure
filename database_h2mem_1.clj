86e5e22220b7dca2bf82582005a7a37e3c1e3543
81784244368 <paulo.silva-junior@serpro.gov.br>
2024-01-21 08:45:27 -0300

    removendo sources que não rodam

--- database_h2mem_1.clj
+++
@@ -1,8 +0,0 @@
-(ns dbname
-    (:require [clojure.java.jdbc :as jdbc]))
-; erro ao pegar clojure.java.jdbc - nao compila - tentar com lein
-(def db-spec {
-              :dbtype "h2:mem"
-              :dbname "mydb" })
-
-(jdbc/query dbspec ["SELECT 3*4 AS result"])
